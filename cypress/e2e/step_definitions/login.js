25d010b825f44e2a8ccb2e7b069022789ba621c0
Fernando Papito <fernandorcks@gmail.com>
2023-11-16 20:48:28 -0300

    codigo final

--- cypress/e2e/step_definitions/login.js
+++ cypress/e2e/step_definitions/login.js
@@ -3,3 +3,48 @@ import {
   When,
   Then,
 } from '@badeball/cypress-cucumber-preprocessor';
+
+Given("que acesso a página de login", function () {
+  cy.visit('https://loginxp.vercel.app')
+});
+
+When("submeto minhas credênciais com {string} e {string}", function (user, password) {
+
+  user
+    ? cy.get('input[name=user]').type(user)
+    : cy.log('username is empty')
+
+  password
+    ? cy.get('input[name=pass]').type(password)
+    : cy.log('password is empty')
+
+  cy.contains('button', 'Entrar').click()
+});
+
+
+Given("submeto minhas credênciais:", function (dataTable) {
+
+  const user = dataTable.rowsHash()
+
+  user.username
+    ? cy.get('input[name=user]').type(user.username)
+    : cy.log('username is empty')
+
+  user.password
+    ? cy.get('input[name=pass]').type(user.password)
+    : cy.log('password is empty')
+
+  cy.contains('button', 'Entrar').click()
+
+});
+
+
+Then("sou autenticado no sistema", function () {
+  cy.get('.swal2-html-container')
+    .should('be.visible')
+    .should('have.text', 'Suas credenciais são válidas :)')
+});
+
+Then("devo ver a notificação {string}", function (notice) {
+  cy.contains(notice)
+});
